c1a70483765ad48a9930d29e5390c62b58665ad4
Ivan Pusic <450140+ivpusic@users.noreply.github.com>
2019-07-28 18:24:27 +0200

    add support for bang prefix expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -23,9 +23,35 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	println(object.Object.Inspect)
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
