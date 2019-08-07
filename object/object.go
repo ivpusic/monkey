a8fd83d7609a1eb73e72de3722b1a53fc2b558a3
Ivan Pusic <450140+ivpusic@users.noreply.github.com>
2019-08-07 22:09:27 +0200

    add len builtin function

--- object/object.go
+++ object/object.go
@@ -18,6 +18,7 @@ const (
 	ERROR_OBJ        = "ERROR"
 	FUNCTION_OBJ     = "FUNCTION"
 	STRING_OBJ       = "STRING"
+	BUILTIN_OBJ      = "BUILTIN"
 )
 
 type Function struct {
@@ -107,5 +108,14 @@ type String struct {
 	Value string
 }
 
+type BuiltinFunction func(args ...Object) Object
+
 func (s *String) Type() ObjectType { return STRING_OBJ }
 func (s *String) Inspect() string  { return s.Value }
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
