23a6f5bb7a9252f0015fe67e64b3d78f97da5d69
Ivan Pusic <450140+ivpusic@users.noreply.github.com>
2019-08-21 00:04:32 +0200

    add puts built in function

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,8 @@
 package evaluator
 
 import (
+	"fmt"
+
 	"github.com/ivpusic/monkey/object"
 )
 
@@ -97,6 +99,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
